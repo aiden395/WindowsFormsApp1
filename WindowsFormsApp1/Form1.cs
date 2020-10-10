fc9d7b97721b1a8e3e4f8a12cdbb3fdcd5ca0e0c
aids <aidenblount1@gmail.com>
2020-10-10 16:19:40 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+ht
+    t+h 
+    h
+    g
+    etgg89iuoensf e'nsu[;f 
+    +_}g]4 -=[global q234
+    w
